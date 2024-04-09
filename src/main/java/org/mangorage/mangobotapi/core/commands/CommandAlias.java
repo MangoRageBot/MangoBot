b3677087f63791131a91d474131b334236b9b48b
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2024-04-08 20:00:58 -0700

    Added some minor docs to CommandAlias

--- src/main/java/org/mangorage/mangobotapi/core/commands/CommandAlias.java
+++ src/main/java/org/mangorage/mangobotapi/core/commands/CommandAlias.java
@@ -26,8 +26,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Ties a List to an object. Used for Aliases for commands.
+ */
 public final class CommandAlias {
     private static final Map<Object, List<String>> ALIASES = new HashMap<>();
+
+    /**
+     * Pass thru strings to get a list back. Creates the list if it doesnt already exist.
+     * Pass an Object as the key, can be a class, or the instance of the command. or anything.
+     * <p>
+     * Recommended to only pass through the Class or Object instance of the command in question.
+     *
+     * @param object
+     * @param aliases
+     * @return
+     */
 
     public static List<String> create(Object object, String... aliases) {
         return ALIASES.computeIfAbsent(object, c -> List.of(aliases));
