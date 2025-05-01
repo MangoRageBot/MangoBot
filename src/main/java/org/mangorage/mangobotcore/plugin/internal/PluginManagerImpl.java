16eb60ac82755af1c19cbe09aa78789797be4eb8
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-05-01 00:34:53 -0700

    Removed bootstrap from module-info.java

--- src/main/java/org/mangorage/mangobotcore/plugin/internal/PluginManagerImpl.java
+++ src/main/java/org/mangorage/mangobotcore/plugin/internal/PluginManagerImpl.java
@@ -49,17 +49,10 @@ public final class PluginManagerImpl implements PluginManager {
                 .enableAllInfo()
                 .scan()) {
 
-            scanResult.getClassesWithAnnotation(MangoBotPlugin.class.getName()).forEach(info -> {
-                loop: for (Module module : PluginManagerImpl.class.getModule().getLayer().modules()) {
-                    try {
-                        var clz = Class.forName(module, info.getName());
-                        if (clz != null) {
-                            scanner.add(clz);
-                            break loop;
-                        }
-                    } catch (Throwable ignored) {}
-                }
-            });
+            List<Class<?>> annotated = scanResult.getClassesWithAnnotation(MangoBotPlugin.class.getName())
+                    .loadClasses();
+
+            scanner.addAll(annotated);
         }
 
         scanner
