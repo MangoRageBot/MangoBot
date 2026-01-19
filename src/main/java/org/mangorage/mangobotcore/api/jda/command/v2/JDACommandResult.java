b5f98ac17b23c8b3561348b4395fbb0b9cc7ee00
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2026-01-18 22:36:55 -0800

    Fixed a few issues.

--- src/main/java/org/mangorage/mangobotcore/api/jda/command/v2/JDACommandResult.java
+++ src/main/java/org/mangorage/mangobotcore/api/jda/command/v2/JDACommandResult.java
@@ -1,10 +1,21 @@
 package org.mangorage.mangobotcore.api.jda.command.v2;
 
 public enum JDACommandResult {
-    PASS,
-    FAIL,
-    ERROR,
-    NO_PERMISSION,
-    INVALID_COMMAND,
-    GUILD_ONLY
+    PASS(null),
+    FAIL("Command execution failed."),
+    ERROR("An error occurred while executing the command."),
+    UNDER_MAINTENANCE("This command is currently under maintenance."),
+    NO_PERMISSION("You do not have permission to execute this command."),
+    INVALID_COMMAND("Invalid command."),
+    GUILD_ONLY("This command can only be used in a guild."),;
+
+    private final String message;
+
+    JDACommandResult(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
