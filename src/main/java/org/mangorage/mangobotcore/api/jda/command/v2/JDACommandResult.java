32bc772d5dc0a5cef112f06d428940903231f39e
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2026-01-19 23:58:27 -0800

    Made it so PASS/ERROR are the only two which do not have a "message", that is handled by commandparseresult now.

--- src/main/java/org/mangorage/mangobotcore/api/jda/command/v2/JDACommandResult.java
+++ src/main/java/org/mangorage/mangobotcore/api/jda/command/v2/JDACommandResult.java
@@ -2,12 +2,12 @@ package org.mangorage.mangobotcore.api.jda.command.v2;
 
 public enum JDACommandResult {
     PASS(null),
-    FAIL("Command execution failed."),
-    ERROR("An error occurred while executing the command."),
-    UNDER_MAINTENANCE("This command is currently under maintenance."),
+    ERROR(null),
+
+    INVALID_COMMAND("Invalid Command"),
     NO_PERMISSION("You do not have permission to execute this command."),
-    INVALID_COMMAND("Invalid command."),
-    GUILD_ONLY("This command can only be used in a guild."),;
+    GUILD_ONLY("This command can only be used in a guild."),
+    UNDER_MAINTENANCE("This command is currently under maintenance.");
 
     private final String message;
 
