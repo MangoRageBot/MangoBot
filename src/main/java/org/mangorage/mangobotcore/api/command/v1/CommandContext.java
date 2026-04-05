c4925978c9e9a4daf8559e50b22f9bc017c2e0ab
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2026-04-05 12:13:41 -0700

    Minor fix for CommandContext

--- src/main/java/org/mangorage/mangobotcore/api/command/v1/CommandContext.java
+++ src/main/java/org/mangorage/mangobotcore/api/command/v1/CommandContext.java
@@ -42,7 +42,7 @@ public final class CommandContext<C> {
     }
 
     public <T> T getArgumentOrElse(OptionalArg<T> optionalArgument) {
-        if (index > arguments.length) // End of arguments...
+        if (index >= arguments.length) // End of arguments...
             return optionalArgument.getDefaultValue();
 
         T result = null;
